5cefb05987a415aa3010054239c727a134cdd88e
Nofil Ali Khan <nofilalikhan@gmail.com>
2026-08-09 23:10:45 +0500

    feat: wire up main entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 //
 // Created by nofilkhan on 7/25/26.
 //
+#include "CommandParser.h"
+#include <iostream>
 
-#include "main.h"
+int main(int argc, char* argv[]) {
+	if (argc < 2){
+		std::cerr << "usage: mingit <command> [args...]\n";
+		return 1;
+	}
+	return CommandParser::run(argc, argv);
+
+}
